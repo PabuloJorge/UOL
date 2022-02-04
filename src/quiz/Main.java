bb5f0f1ba43a23cfc9524ac09a4862973aa8dc79
pabulojorge <pabulo_dias@hotmail.com>
2022-02-04 12:05:48 -0300

    Desafio do quiz finalizado

--- src/quiz/Main.java
+++ src/quiz/Main.java
@@ -1,9 +1,41 @@
 package quiz;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String nome;
+		int acertos = 0, erros = 0;
+		Scanner leia = new Scanner(System.in);
+
+		ArrayList<Pergunta> perguntas = new ArrayList();
+		perguntas.add(new Pergunta("Quanto é 4x4?", "16"));
+		perguntas.add(new Pergunta("Com quantos anos morreu Albert Einstein?", "76"));
+
+		System.out.println("Digite seu nome:");
+		nome = leia.nextLine();
+
+		for (int i = 0; i < perguntas.size(); i++) {
+			System.out.println(perguntas.get(i).getEnunciado());
+			System.out.println(perguntas.get(i).getResposta());
+			String resposta = leia.nextLine();
+			if (resposta.equals(perguntas.get(i).getResposta())) {
+				System.out.println("PARABÉNS, VOCÊ ACERTOU :D");
+				acertos++;
+			} else {
+				System.out.println("QUE PENA, A RESPOSTA ESTÁ ERRADA :/");
+				erros++;
+			}
+			System.out.println("====================================================");
+		}
+
+		System.out.println("RESULTADO");
+		System.out.println("Usuário: " + nome);
+		System.out.println("Acertos: " + acertos);
+		System.out.println("Erros: " + erros);
 
 	}
 
---
+++ src/quiz/Pergunta.java
@@ -0,0 +1,28 @@
+package quiz;
+
+public class Pergunta {
+	private String enunciado;
+	private String resposta;
+
+	public Pergunta(String enunciado, String resposta) {
+		this.enunciado = enunciado;
+		this.resposta = resposta;
+	}
+
+	public String getEnunciado() {
+		return enunciado;
+	}
+
+	public void setEnunciado(String enunciado) {
+		this.enunciado = enunciado;
+	}
+
+	public String getResposta() {
+		return resposta;
+	}
+
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
+
+}
